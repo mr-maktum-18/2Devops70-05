253d3ce5ebc719cfbb1b0fed565b663f8199ebff
mr-maktum-18 <naktumgudubhai@gamil.com>
2025-05-07 15:20:48 +0530

    save

---
+++ DivisionOperation.java
@@ -0,0 +1,12 @@
+public class DivisionOperation {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 3;
+        try{
+            int div = a / b;
+            System.out.println("devision: "+div);
+        }catch(ArithmeticException e){
+            System.out.println("Error: cannot divide by zero");
+        }
+    }
+}
